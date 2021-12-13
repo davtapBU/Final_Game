aa7f75403dc3f3d3090d3b58654cbd91826adb86
David Tapia <dtapiap1@binghamton.edu>
2021-12-13 04:07:23 -0500

    Made notes fall to the rhythm of the music

--- FNF/BeatMap.swift
+++ FNF/BeatMap.swift
@@ -8,20 +8,20 @@ import UIKit
 
 struct Note {
     // Properties of any given note
-    let direction: CGFloat
-    let length: CGFloat
+    let direction: Int
+    let length: Int
     
     // Whether a note needs be held down or not
     let held: Bool
     // Value and length for hold note i.e Held length = half, HeldNum = 3 --> Hold note for 3 half notes
-    let heldLength: CGFloat
-    let heldNum: CGFloat
+    let heldLength: Int
+    let heldNum: Int
 }
 
 struct BeatMap {
     
     // Beats per minute -- Used to determine delay between notes
-    let bpm: CGFloat
+    let bpm: Double
     
     // Name to be displayed as song name
     let songName: String
@@ -30,7 +30,7 @@ struct BeatMap {
     let fileName: String
     
     // Time delay before first note of song
-    let startDelay: CGFloat
+    let startDelay: Double
     
     var notes: [Note]
 }
@@ -39,18 +39,18 @@ struct BeatMap {
 
 //Direction will tell us where to place note and what button it will need to interact with
 enum directions{
-    static let Left: CGFloat = 0
-    static let Right: CGFloat = 1
-    static let Up: CGFloat = 2
-    static let Down: CGFloat = 3
+    static let Left: Int = 0
+    static let Right: Int = 1
+    static let Up: Int = 2
+    static let Down: Int = 3
 }
 
 //Length will tell us how
 enum lengths{
-    static let Whole: CGFloat = 0
-    static let Half: CGFloat = 1
-    static let Quarter: CGFloat = 2
-    static let Eighth: CGFloat = 3
-    static let Sixteenth: CGFloat = 4
-    static let ThirtySecond: CGFloat = 5
+    static let Whole: Int = 0
+    static let Half: Int = 1
+    static let Quarter: Int = 2
+    static let Eighth: Int = 3
+    static let Sixteenth: Int = 4
+    static let ThirtySecond: Int = 5
 }
--- FNF/GameScene.swift
+++ FNF/GameScene.swift
@@ -7,6 +7,7 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
 class GameScene: SKScene {
     
@@ -14,11 +15,12 @@ class GameScene: SKScene {
     var graphs = [String : GKGraph]()
     
     private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-
+    
+    var music: AVAudioPlayer!
+    var firstNote: Bool = true
+    
     override func sceneDidLoad() {
-
+        playSong(map: ttls)
     }
     
     
@@ -43,7 +45,6 @@ class GameScene: SKScene {
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
     }
     
-    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
@@ -65,50 +66,80 @@ class GameScene: SKScene {
     
     func playSong(map: BeatMap)
     {
-        // Play the music
-        let backgroundSound = SKAudioNode(fileNamed: map.fileName);
-        self.addChild(backgroundSound)
-        
-        // Wait the set delay before queing notes
-        DispatchQueue.main.asyncAfter(deadline: .now() + map.startDelay) {
-            // Initialize time for queing
-            var curTime : DispatchTime = .now()
-            
-            // Calculate bps
-            let bps : CGFloat = map.bpm/60.0
-            
-            // Queue notes and increase dispatch delay of next note depending on current note length
-            map.notes.forEach { note in
-                switch(note.length)
-                {
-                case lengths.Whole:
-                    self.queueNote(dline: curTime, note: note)
-                    curTime = curTime + (bps*4)
-                case lengths.Half:
-                    self.queueNote(dline: curTime, note: note)
-                    curTime = curTime + (bps*2)
-                case lengths.Quarter:
-                    self.queueNote(dline: curTime, note: note)
-                    curTime = curTime + bps
-                case lengths.Eighth:
-                    self.queueNote(dline: curTime, note: note)
-                    curTime = curTime + (bps/2)
-                case lengths.Sixteenth:
-                    self.queueNote(dline: curTime, note: note)
-                    curTime = curTime + (bps/4)
-                case lengths.ThirtySecond:
-                    self.queueNote(dline: curTime, note: note)
-                    curTime = curTime + (bps/8)
-                default: break
-                }
+        if let musicURL = Bundle.main.url(forResource: "ttls", withExtension: "m4a") {
+            if let audioPlayer = try? AVAudioPlayer(contentsOf: musicURL) {
+                music = audioPlayer
+                music.numberOfLoops = 0
+                music.play()
+                print("playing...")
+            }
+            else {
+                print("error!!")
             }
         }
+        
+        queueNotes(map: map)
     }
     
-    func queueNote(dline: DispatchTime, note: Note)
-    {
-        DispatchQueue.main.asyncAfter(deadline: dline) {
+    @objc func queueNote(timer: Timer) {
+        print("called!")
+        let sprite = SKSpriteNode(color: UIColor.red, size: CGSize(width:40, height:40))
+        sprite.texture = SKTexture(imageNamed: "playnote")
+        sprite.setScale(3.0)
+        let dir : Int = (timer.userInfo as? Int)!
+        
+        switch(dir){
+        case directions.Left:
+            sprite.position = CGPoint(x: -220, y: screen_height)
+        case directions.Down:
+            sprite.position = CGPoint(x: -110, y: screen_height)
+        case directions.Up:
+            sprite.position = CGPoint(x: 110, y: screen_height)
+        case directions.Right:
+            sprite.position = CGPoint(x: 220, y: screen_height)
+        default:
+            break
+        }
+        
+        
+        addChild(sprite)
+        
+        let movement = SKAction.move(by: CGVector(dx:0, dy:2 * -screen_height), duration: 2)
+        let removal = SKAction.removeFromParent()
+        
+        sprite.run(SKAction.sequence([movement, removal]))
+        
+    }
+    
+    func queueNotes(map: BeatMap) {
+        // Calculate bps
+        let bps : Double = (map.bpm)/60.0
+        
+        // Delay between notes
+        var delay: Double = map.startDelay
+        
+        // Queue notes and increase dispatch delay of next note depending on current note length
+        map.notes.forEach { note in
             
+            let context = note.direction
+            Timer.scheduledTimer(timeInterval: delay, target: self, selector: #selector(queueNote), userInfo: context, repeats: false)
+            
+            switch(note.length)
+            {
+            case lengths.Whole:
+                delay += bps * 4
+            case lengths.Half:
+                delay += bps * 2
+            case lengths.Quarter:
+                delay += bps * 1
+            case lengths.Eighth:
+                delay += bps / 2
+            case lengths.Sixteenth:
+                delay += bps / 4
+            case lengths.ThirtySecond:
+                delay += bps / 8
+            default: break
+            }
         }
     }
 }
